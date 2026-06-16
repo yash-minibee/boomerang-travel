bd3f03c44170bbeb863e744e3c7116dd277cfff5
Yash Vadhavana <yashvadhavana108@gmail.com>
2026-06-16 12:17:31 +0530

    vercel fix-2

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/admin",
+  base: "/admin/",
 })
